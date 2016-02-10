c67e9e7af871e1c2ef272fbfb652b9ba1e32b0a7
akkig <ankur.gpt28@gmail.com>
2016-02-10 12:01:23 -0600

    fix: shared message pass issue

--- src/Process.java
+++ src/Process.java
@@ -2,6 +2,8 @@ public class Process implements Runnable {
 	
 	public Message leftInBuffer;
 	public Message rightInBuffer;
+	public Message leftOutBuffer;
+	public Message rightOutBuffer;
 
 	
 	public int id;
@@ -33,28 +35,41 @@ public class Process implements Runnable {
 		int phase = 0;
 		int max_phase = (int)(Math.log(HS_Algorithm.num_process) / Math.log(2));
 
-		while((phase <= max_phase) && !HS_Algorithm.leaderElected) { 
+		while((phase <= max_phase+1) && !HS_Algorithm.leaderElected) { 
 			try {
 				HS_Algorithm.phaseStarted.acquire();
+				Thread.sleep(100); // To ensure all have acquired the semaphore
+
 				this.leftInBuffer = null;
 				this.rightInBuffer = null;
-				Thread.sleep(100); // To ensure all have acquired the semaphore
+				this.leftOutBuffer = null;
+				this.rightOutBuffer = null;
 				
-				int hops = (int)Math.pow(2, phase);
+				int max_possible_hops = (int)Math.pow(2, phase);
 				
-				Message message = generateMessage(leaderStatus, hops-1);
-				int round = 1; 
+				int hops =  (max_possible_hops > HS_Algorithm.num_process)
+						  ? HS_Algorithm.num_process : max_possible_hops ;
+				
+				int round = 1;
 				while ((round <= 2*hops) && !HS_Algorithm.leaderElected) {
 					HS_Algorithm.roundStarted.acquire();
-					Thread.sleep(100);
-					
-					if (round == 1 && leaderStatus.equals(LeaderStatus.UNKNOWN)) {
-						sendExplore(message);
-					} else {
-						passMessage(leaderStatus);
-					}
-					
+					Thread.sleep(100); // To ensure all have acquired the semaphore
+
 					System.out.println("phase: " + phase + " round: "+round + " id: "+ this.id);
+
+					if (round == 1 && this.leaderStatus.equals(LeaderStatus.UNKNOWN)) {
+						sendExplore(hops);
+					} else if (round == 1 && this.leaderStatus.equals(LeaderStatus.NON_LEADER)) {
+						//sendExplore(message);
+					} else  {
+						
+						HS_Algorithm.processes[this.left_index].getMessages(this, "l");
+						HS_Algorithm.processes[this.right_index].getMessages(this, "r");
+						
+						Thread.sleep(100);// To synchronize: Each thread had fetched the message
+						
+						processMessage(leaderStatus);					
+					}
 					
 					HS_Algorithm.roundCompleted.acquire();
 					if (HS_Algorithm.roundCompleted.availablePermits() == 0) {
@@ -70,137 +85,147 @@ public class Process implements Runnable {
 					HS_Algorithm.phaseStarted.release(HS_Algorithm.num_process);
 				}
 				phase++;
+				
+				
 			} catch (InterruptedException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
 			}
 		}
 	}
-	
-	
-	public void putMessageInBuffer(Message message, String direction) {
-		if(direction.equals("l")) {
-			if (leftInBuffer != null) {
-				if (leftInBuffer.message < message.message) {
-
-				} else if (leftInBuffer.message > message.message) {
-					leftInBuffer = message;
-				}
-			} else {
-				leftInBuffer = message;
-			}
-		} else if (direction.equals("r")) {
-			if (rightInBuffer != null) {
-				if (rightInBuffer.message < message.message) {
-				} else if (rightInBuffer.message > message.message) {
-					rightInBuffer = message;
-				}
-			} else {
-				rightInBuffer = message;
-			}
-		}
-	}
-	
-	
+		
 	public void updateStatus(LeaderStatus leaderStatus, Message message) {
-		if(leaderStatus.equals(LeaderStatus.UNKNOWN)) {
+		if(this.leaderStatus.equals(LeaderStatus.UNKNOWN)) {
 			if (message.type.equals(Message.MessageType.EXPLORE)) {
 				if (message.message == this.id) {
 					this.leaderStatus = LeaderStatus.LEADER;
-					System.out.println("LEADER: "+ this.id);
+					System.out.println("LEADER------------------------------------------------------: "+ this.id);
 					HS_Algorithm.leaderElected = true;
-				}
-				else if(message.message < this.id) {
-					leaderStatus = LeaderStatus.NON_LEADER;
 				}
 			}
 		}
 	}
-	public void passMessage(LeaderStatus leaderStatus) {
+	
+	public void getMessages(Process p, String direction) {
+		if(direction.equals("l")) {
+			p.leftInBuffer = this.rightOutBuffer;
+			this.rightOutBuffer = null;
+		}
+		if(direction.equals("r")) {
+			p.rightInBuffer = this.leftOutBuffer;
+			this.leftOutBuffer = null;
+		}
+	}
+	
+	public void processMessage(LeaderStatus leaderStatus) {
 		
-		if(leftInBuffer != null) {
-			Message message = leftInBuffer;
-			//System.out.println(message.hops_left + " : " + message.message +" : "+ this.id+"l");
-			updateStatus(leaderStatus, message);
-			if(message.hops_left == 0) {
-				message.hops_left = message.hops_left - 1;
-				message.type = Message.MessageType.ACK;				
-				HS_Algorithm.processes[this.left_index].putMessageInBuffer(message, "r");;	
-			
-				System.out.println(this.id +"->"
-				+ HS_Algorithm.processes[this.left_index].id 
-				+ ": "+message.message
-				+ " ACK "
-				+ " Hop "+ message.hops_left);
-				
-			} else {
-				if (this.id == message.message) {	
-					System.out.println(this.id +"=="+ this.id);
-
-				} else {
+		if(this.leftInBuffer != null) {
+			Message message = this.leftInBuffer;
+			if (message.message == this.id) {
+				this.leftOutBuffer = null;
+				this.rightOutBuffer = null;
+			} else if (message.message < this.id) {
+				this.leaderStatus = LeaderStatus.NON_LEADER;
+				System.out.println(this.id + "---->"+this.leaderStatus.toString());
+				if(message.hops_left == 0) {
 					message.hops_left = message.hops_left - 1;
-					
-					HS_Algorithm.processes[this.right_index].putMessageInBuffer(message, "l");;
-					
+					message.type = Message.MessageType.ACK;
+
+					System.out.println("message:" + message.message + "--------------> ACK");
+
+					this.leftOutBuffer = message;
+					System.out.println(this.id +"->"
+							+ HS_Algorithm.processes[this.left_index].id 
+							+ ": "+message.message
+							+ " ACK "
+							+ " Hop "+ message.hops_left
+							+ "  "+ message.type.toString());
+				} else {
+					message.hops_left = message.hops_left - 1;	
+					this.rightOutBuffer = message;
+
 					System.out.println(this.id +"->"
 							+ HS_Algorithm.processes[this.right_index].id 
 							+ ": "+message.message
 							+ " ACK- "
-							+ " Hop "+ message.hops_left);
+							+ " Hop "+ message.hops_left
+							+ "  "+ message.type.toString());
 				}
-			}
-			if (message.equals(leftInBuffer)) {
-				leftInBuffer = null;
-			}
-		}
-		
-		if (rightInBuffer != null) {
-			Message message = rightInBuffer;
-			//System.out.println(message.hops_left + " : " + message.message +" : "+ this.id+"r");
-			updateStatus(leaderStatus, message);
-			
-			if(message.hops_left == 0) {
-				message.type = Message.MessageType.ACK;
-				message.hops_left = message.hops_left - 1;
-
-				HS_Algorithm.processes[this.right_index].putMessageInBuffer(message, "l");
-
-				System.out.println(this.id +"->"
-						+ HS_Algorithm.processes[this.right_index].id 
-						+ ": "+message.message
-						+ " ACK-- "
-						+ " Hop "+ message.hops_left);
 			} else {
-				if (this.id == message.message) {
-					System.out.println(this.id +"=="+ this.id);
+				this.rightOutBuffer = null;
+			}
+			updateStatus(this.leaderStatus, message);
+			this.leftInBuffer = null;			
+		}
 
+		if (this.rightInBuffer != null) {
+			Message message = this.rightInBuffer;
+			if (message.message == this.id) {
+				this.leftOutBuffer = null;
+				this.rightOutBuffer = null;
+			} else if (message.message < this.id){
+				this.leaderStatus = LeaderStatus.NON_LEADER;
+				System.out.println(this.id + "---->"+this.leaderStatus.toString());
+
+				if(message.hops_left == 0) {
+					message.type = Message.MessageType.ACK;
+					message.hops_left = message.hops_left - 1;
+					System.out.println("message:" + message.message + "--------------> ACK");
+
+					this.rightOutBuffer = message;
+
+					System.out.println(this.id +"->"
+							+ HS_Algorithm.processes[this.right_index].id 
+							+ ": "+message.message
+							+ " ACK-- "
+							+ " Hop "+ message.hops_left
+							+ "  "+ message.type.toString());
 				} else {
 					message.hops_left = message.hops_left - 1;
-					HS_Algorithm.processes[this.left_index].putMessageInBuffer(message, "r");;
+					this.leftOutBuffer = message;
 
 					System.out.println(this.id +"->"
 							+ HS_Algorithm.processes[this.left_index].id 
 							+ ": "+message.message
 							+ " ACK--- "
-							+ " Hop "+ message.hops_left);
+							+ " Hop "+ message.hops_left
+							+ "  "+ message.type.toString());
 				}
+				
+			} else {
+				this.leftOutBuffer = null;
 			}
-			if (message.equals(rightInBuffer)) {
-				rightInBuffer = null;
-			}
-
+			updateStatus(this.leaderStatus, message);
+			this.rightInBuffer = null;
 		}
+		
 	}
 	
-	public void sendExplore(Message message) {
-		HS_Algorithm.processes[this.left_index].putMessageInBuffer(message, "r");
-		HS_Algorithm.processes[this.right_index].putMessageInBuffer(message, "l");
+	public void sendExplore(int hops) {
+
+		Message msgleft = generateMessage(LeaderStatus.UNKNOWN, hops-1);
+		Message msgright = generateMessage(LeaderStatus.UNKNOWN, hops-1);
+
+		this.leftOutBuffer = msgleft;
+		this.rightOutBuffer = msgright;
+		
+		System.out.println(this.id +"->"
+				+ HS_Algorithm.processes[this.left_index].id 
+				+ ": "+msgleft.message
+				+ " EXP "
+				+ " Hop "+ msgleft.hops_left
+				+ "  "+ msgleft.type.toString());
+		System.out.println(this.id +"->"
+				+ HS_Algorithm.processes[this.right_index].id 
+				+ ": "+msgright.message
+				+ " EXP "
+				+ " Hop "+ msgright.hops_left
+				+ "  "+ msgright.type.toString());
 	}
 	public Message generateMessage(LeaderStatus leaderStatus, int hops) {	
 		Message message = new Message(Message.MessageType.EXPLORE, this.id, hops);
 
 		return message;
 	}
-	
 	
 }
